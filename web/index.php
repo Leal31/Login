b42417f0c0128904b0851b35c2d109bcfb74a389
Leal31 <lealemanuel31@protonmail.com>
2022-05-26 17:39:02 -0500

    l

--- web/index.php
+++ web/index.php
@@ -1,14 +1,18 @@
 <?php
 include_once '../lib/helpers.php';
 include_once '../view/partials/headers.php';
-include_once '../view/partials/navbar.php';
-
-if (isset($_GET['modulo'])) {
-  $modulo = $_GET['modulo'];
-  $controlador = $_GET['controlador'];
-  $funcion = $_GET['funcion'];
-  getUrl($modulo, $controlador, $funcion);
-  resolve();
+session_start();
+if (isset($_SESSION['documento'])) {
+  include_once '../view/partials/navbar.php';
+  if (isset($_GET['modulo'])) {
+    $modulo = $_GET['modulo'];
+    $controlador = $_GET['controlador'];
+    $funcion = $_GET['funcion'];
+    getUrl($modulo, $controlador, $funcion);
+    resolve();
+  }
+} else {
+  include '../view/formSesion.php';
 }
 
 include_once '../view/partials/footers.php';
